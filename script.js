08fcfd0b4a479830f5e4c94b4c5bfad1152f82d0
Ebartho <arvid@thoresson.net>
2026-03-25 13:19:36 +0100

    responsive
    gjort responsive

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-let wrapper = $("#wrapper")
+let wrapper = $("#wrapper");
 
 $("#husplustext" ).hide();
-wrapper.css('height', '100vh')
-wrapper.css('grid-template-rows', '15% 70% 15%')
+wrapper.css('height', '100vh');
 wrapper.css('grid-template-areas', 'rubrik husbild info')
+wrapper.css('grid-template-rows', '15% 75% 0% 10%')
 if($( document ).width() <= '1200'){
     wrapper.css('height', '220vh')
-    wrapper.css('grid-template-rows', '10% 80% 10%')
+    wrapper.css('grid-template-rows', '10% 80% 0% 10%')
     $("#huswrapper").css('flex-direction', 'column')
     $("#huswrapper").css('justify-content', 'center')
     $("#huswrapper").css('align-items', 'center')
     $("#huswrapper").css('margin-bottom', '0px')
+    $("#rubrik").css('width', '80%')
 
 }
 
